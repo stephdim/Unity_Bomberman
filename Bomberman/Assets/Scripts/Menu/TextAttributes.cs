4785f608b185850b26eee2c11ac77dbfa6822ee9
Hyro Vitaly Protago <musty.magic@gmail.com>
2014-11-06 23:39:03 +0100

    Use of Screen.height instead of Screen.width

--- Bomberman/Assets/Scripts/Menu/TextAttributes.cs
+++ Bomberman/Assets/Scripts/Menu/TextAttributes.cs
@@ -1,27 +1,45 @@
 using UnityEngine;
 using System.Collections;
 
-public class TextAttributes : MonoBehaviour
-{
-	
+public class TextAttributes : MonoBehaviour {
+
 	public GUIStyle title_style;
 	public GUIStyle button_style;
 
-	void Update() {
-		
-	}
-
-
 	void OnGUI () {
+		GUI.Label(
+			new Rect (
+				0.4f * Screen.width,
+				0.1f * Screen.height,
+				0.2f * Screen.width,
+				0.3f * Screen.height
+			), "BomberBoy", this.title_style
+		);
 
-		GUI.Label (new Rect (0.4f * Screen.width,0.6f * Screen.width/10,0.2f * Screen.width,0.3f * Screen.height), "BomberBoy", this.title_style);
+		if (GUI.Button(
+				new Rect(
+					0.4f * Screen.width,
+					0.45f * Screen.height,
+					0.2f * Screen.width,
+					this.button_style.fontSize
+				), "Jouer", this.button_style)
+			) {
 
-		if(GUI.Button(new Rect(0.4f * Screen.width,2.1f * Screen.width/10,0.2f * Screen.width,this.button_style.fontSize), "Jouer", this.button_style)) {
 			Application.LoadLevel("Bomberman");
+
 		}
 
-		if(GUI.Button(new Rect(0.4f * Screen.width,2.6f * Screen.width/10,0.2f * Screen.width,this.button_style.fontSize), "Quitter", this.button_style)) {
+		if (GUI.Button(
+				new Rect(
+					0.4f * Screen.width,
+					0.7f * Screen.height,
+					0.2f * Screen.width,
+					this.button_style.fontSize
+				), "Quitter", this.button_style)
+			) {
+
 			Application.Quit();
+
 		}
 	}
 }
